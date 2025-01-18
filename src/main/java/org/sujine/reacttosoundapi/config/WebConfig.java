155d83e00aca52645abbcd7acf940da80033cec2
sujine2 <hwa.sujin1219@gmail.com>
2025-01-18 19:59:03 +0900

    fix: fix CORS

--- src/main/java/org/sujine/reacttosoundapi/config/WebConfig.java
+++ src/main/java/org/sujine/reacttosoundapi/config/WebConfig.java
@@ -1,2 +1,24 @@
-package org.sujine.reacttosoundapi.config;public class WebConfig {
+package org.sujine.reacttosoundapi.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
+
